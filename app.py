d75ad70f3b0dc1633d0188c089ffdaf18e84664b
Buzovskyi Vitalii <buzovskiy.v@gmail.com>
2021-03-09 00:18:18 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,42 @@
+import sys
+import time
+from pywinauto.keyboard import send_keys
+from pywinauto.application import Application
+from pywinauto import Desktop
+from pywinauto.findwindows import WindowAmbiguousError, WindowNotFoundError, ElementAmbiguousError, ElementNotFoundError
+import clipboard
+import warnings
+
+
+warnings.simplefilter('ignore', category=UserWarning)
+windows = Desktop(backend="uia").windows()
+
+app = Application()
+try:
+    app.connect(title_re=".*(?i)(chrome).*")
+except ElementAmbiguousError as e:
+    print('Ошибка: запущено более одного браузера Chrome: ', e)
+    sys.exit("Программа завершена с ошибками")
+except ElementNotFoundError as e:
+    print('Ошибка: окно браузера Chrome не найдено: ', e)
+    sys.exit("Программа завершена с ошибками")
+
+# Access app window object
+app_dialog = app.window()
+app_dialog.set_focus()
+time.sleep(2)
+send_keys('^{TAB}')
+send_keys('^l^c')
+text = clipboard.paste()
+print(text)
+time.sleep(2)
+send_keys('^{TAB}')
+send_keys('^l^c')
+text = clipboard.paste()
+print(text)
+time.sleep(2)
+send_keys('^{TAB}')
+send_keys('^l^c')
+text = clipboard.paste()
+print(text)
+
